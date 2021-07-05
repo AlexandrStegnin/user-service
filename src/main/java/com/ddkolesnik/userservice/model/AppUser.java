d4071898018948eb4e8f3fc63fdd8ec6fb91597a
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-07-05 22:00:26 +0500

    Добавлен класс пользователь системы

---
+++ src/main/java/com/ddkolesnik/userservice/model/AppUser.java
@@ -0,0 +1,20 @@
+package com.ddkolesnik.userservice.model;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * @author Aleksandr Stegnin on 05.07.2021
+ */
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AppUser {
+
+  String name;
+  String surname;
+  String patronymic;
+  String email;
+  String phone;
+
+}
