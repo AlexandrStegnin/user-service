7ab543c416346656d8e6ab4d5c4ef003bf14f742
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-07-06 00:07:03 +0500

    Добавлен класс для получения информации о списке контактов

---
+++ src/main/java/com/ddkolesnik/userservice/model/ContactList.java
@@ -0,0 +1,21 @@
+package com.ddkolesnik.userservice.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * @author Aleksandr Stegnin on 05.07.2021
+ */
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ContactList {
+
+  List<Contact> result = new ArrayList<>();
+  String error;
+  String total;
+  String next;
+
+}
