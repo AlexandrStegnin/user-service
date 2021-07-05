76571a5575fc3cd9c93b81240e5883acc30ea42d
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-07-06 00:04:15 +0500

    Добавлен класс для получения информации об email

---
+++ src/main/java/com/ddkolesnik/userservice/model/Email.java
@@ -0,0 +1,38 @@
+package com.ddkolesnik.userservice.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * @author Aleksandr Stegnin on 05.07.2021
+ */
+@Data
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Email {
+
+  @JsonProperty("VALUE_TYPE")
+  String valueType;
+
+  @JsonProperty("VALUE")
+  String value;
+
+  @JsonProperty("TYPE_ID")
+  String typeId;
+
+  @JsonCreator
+  public Email(@JsonProperty("VALUE_TYPE") String valueType,
+               @JsonProperty("VALUE") String value,
+               @JsonProperty("TYPE_ID") String typeId) {
+    this.valueType = valueType;
+    this.value = value;
+    this.typeId = typeId;
+  }
+
+}
