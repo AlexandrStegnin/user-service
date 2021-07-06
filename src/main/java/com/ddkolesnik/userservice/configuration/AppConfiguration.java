5347cc7d90a2172830223c602a6f9cc98dd202f5
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-07-06 22:13:09 +0500

    Вынесены настройки клиента в файл конфигурации.

--- src/main/java/com/ddkolesnik/userservice/configuration/AppConfiguration.java
+++ src/main/java/com/ddkolesnik/userservice/configuration/AppConfiguration.java
@@ -1,18 +1,46 @@
 package com.ddkolesnik.userservice.configuration;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 /**
  * @author Aleksandr Stegnin on 05.07.2021
  */
 @Configuration
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@PropertySource(value = {"classpath:private.properties", "classpath:application.properties"})
 public class AppConfiguration {
+
+  @Value("${bitrix.default.url}")
+  String BITRIX_API_BASE_URL;
+
+  @Value("${bitrix.webhook.user.id}")
+  String BITRIX_WEBHOOK_USER_ID;
+
+  @Value("${bitrix.access.key}")
+  String BITRIX_ACCESS_KEY;
 
   @Bean
   public RestTemplate getRestTemplate() {
-    return new RestTemplate();
+    RestTemplateBuilder builder = new RestTemplateBuilder();
+    DefaultUriBuilderFactory builderFactory = new DefaultUriBuilderFactory(getBaseUrl());
+    builder.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+    RestTemplate restTemplate = builder.build();
+    restTemplate.setUriTemplateHandler(builderFactory);
+    return restTemplate;
+  }
+
+  private String getBaseUrl() {
+    return BITRIX_API_BASE_URL + BITRIX_WEBHOOK_USER_ID + BITRIX_ACCESS_KEY;
   }
 
 }
--- src/main/java/com/ddkolesnik/userservice/service/BitrixContactService.java
+++ src/main/java/com/ddkolesnik/userservice/service/BitrixContactService.java
@@ -16,9 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -32,15 +30,6 @@ import org.springframework.web.client.RestTemplate;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @PropertySource(value = {"classpath:private.properties", "classpath:application.properties"})
 public class BitrixContactService {
-
-  @Value("${bitrix.default.url}")
-  String BITRIX_API_BASE_URL;
-
-  @Value("${bitrix.webhook.user.id}")
-  String BITRIX_WEBHOOK_USER_ID;
-
-  @Value("${bitrix.access.key}")
-  String BITRIX_ACCESS_KEY;
 
   @Value("${bitrix.crm.duplicate.findbycomm}")
   String BITRIX_CRM_DUPLICATE_FIND_BY_COMM;
@@ -67,21 +56,18 @@ public class BitrixContactService {
 
   @Autowired
   public BitrixContactService(RestTemplate restTemplate) {
-    HttpHeaders httpHeaders = new HttpHeaders();
-    httpHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
     this.restTemplate = restTemplate;
     this.duplicateFilter = new DuplicateFilter();
     this.contactListFilter = new ContactListFilter();
     this.updateContact = new UpdateContact();
-    this.httpEntity = new HttpEntity<>(duplicateFilter, httpHeaders);
-    this.contactListEntity = new HttpEntity<>(contactListFilter, httpHeaders);
-    this.contactHttpEntity = new HttpEntity<>(updateContact, httpHeaders);
+    this.httpEntity = new HttpEntity<>(duplicateFilter);
+    this.contactListEntity = new HttpEntity<>(contactListFilter);
+    this.contactHttpEntity = new HttpEntity<>(updateContact);
   }
 
   public DuplicateResult findDuplicatesByPhones(List<String> phones) {
     duplicateFilter.setValues(phones);
-    ResponseEntity<DuplicateResult> bitrixResult = restTemplate.exchange(
-        BITRIX_API_BASE_URL + BITRIX_WEBHOOK_USER_ID + BITRIX_ACCESS_KEY + BITRIX_CRM_DUPLICATE_FIND_BY_COMM,
+    ResponseEntity<DuplicateResult> bitrixResult = restTemplate.exchange(BITRIX_CRM_DUPLICATE_FIND_BY_COMM,
         HttpMethod.POST, httpEntity, DuplicateResult.class);
     DuplicateResult duplicate = bitrixResult.getBody();
     log.info("Результат поиска дубликатов {}", duplicate);
@@ -92,8 +78,7 @@ public class BitrixContactService {
     Map<String, String[]> filter = new HashMap<>();
     filter.put("PHONE", phones.toArray(new String[0]));
     contactListFilter.setFilter(filter);
-    ResponseEntity<ContactList> contactList = restTemplate.exchange(
-        BITRIX_API_BASE_URL + BITRIX_WEBHOOK_USER_ID + BITRIX_ACCESS_KEY + BITRIX_CRM_CONTACT_LIST,
+    ResponseEntity<ContactList> contactList = restTemplate.exchange(BITRIX_CRM_CONTACT_LIST,
         HttpMethod.POST, contactListEntity, ContactList.class);
     ContactList contacts = contactList.getBody();
     log.info("Результат поиска списка контактов по телефону {}", contacts);
@@ -103,8 +88,7 @@ public class BitrixContactService {
   public Object updateContact(UpdateContact contact) {
     this.updateContact.setId(contact.getId());
     this.updateContact.setFields(contact.getFields());
-    ResponseEntity<Object> update = restTemplate.exchange(
-        BITRIX_API_BASE_URL + BITRIX_WEBHOOK_USER_ID + BITRIX_ACCESS_KEY + BITRIX_CRM_CONTACT_UPDATE,
+    ResponseEntity<Object> update = restTemplate.exchange(BITRIX_CRM_CONTACT_UPDATE,
         HttpMethod.POST, contactHttpEntity, Object.class);
     Object updated = update.getBody();
     log.info("Результат обновления контакта {}", updated);
