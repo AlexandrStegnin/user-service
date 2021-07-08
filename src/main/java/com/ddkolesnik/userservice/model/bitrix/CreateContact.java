6ae00bde71a50bfb8962c8f7d5a06a99f280817b
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-07-08 18:47:16 +0500

    Добавлен класс для создания контакта в Б24.

---
+++ src/main/java/com/ddkolesnik/userservice/model/bitrix/CreateContact.java
@@ -0,0 +1,25 @@
+package com.ddkolesnik.userservice.model.bitrix;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * @author Aleksandr Stegnin on 08.07.2021
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CreateContact {
+
+  @Builder.Default
+  Map<String, Object> fields = new LinkedHashMap<>();
+
+}
