f816a28f3d195a9f148f01b822496db8b68ac70d
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-07-10 12:13:21 +0500

    Добавлена конфигурация

---
+++ src/main/java/com/ddkolesnik/userservice/configuration/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.ddkolesnik.userservice.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Aleksandr Stegnin on 08.07.2021
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .authorizeRequests()
+        .antMatchers("/", "/registration").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .logout()
+        .permitAll();
+  }
+
+}
