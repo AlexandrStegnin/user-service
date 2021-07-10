8597db21f587452ae31635ba5fd9e3067753f2e7
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-07-10 12:30:26 +0500

    Добавлен контроллер и форма для регистрации нового инвестора

---
+++ src/main/java/com/ddkolesnik/userservice/controller/RegistrationController.java
@@ -0,0 +1,29 @@
+package com.ddkolesnik.userservice.controller;
+
+import com.ddkolesnik.userservice.model.UserDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * @author Aleksandr Stegnin on 08.07.2021
+ */
+@Controller
+public class RegistrationController {
+
+  @GetMapping(path = "registration")
+  public String registrationPage(Model model) {
+    model.addAttribute("userDTO", new UserDTO());
+    return "registration";
+  }
+
+  @PostMapping(path = "registration")
+  public String register(@ModelAttribute UserDTO userDTO, Model model) {
+    model.addAttribute("userDTO", userDTO);
+    model.addAttribute("success", "Ваши данные отправлены на проверку");
+    return "registration";
+  }
+
+}
