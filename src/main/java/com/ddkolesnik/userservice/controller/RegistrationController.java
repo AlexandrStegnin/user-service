81282c72d486a2e4b9cb739d3384147517ba6a51
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-07-11 17:28:41 +0500

    Добавлен метод подтсерждения номера телефона

--- src/main/java/com/ddkolesnik/userservice/controller/RegistrationController.java
+++ src/main/java/com/ddkolesnik/userservice/controller/RegistrationController.java
@@ -1,11 +1,16 @@
 package com.ddkolesnik.userservice.controller;
 
+import com.ddkolesnik.userservice.model.ConfirmPhoneDTO;
 import com.ddkolesnik.userservice.model.UserDTO;
+import com.ddkolesnik.userservice.response.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author Aleksandr Stegnin on 08.07.2021
@@ -24,6 +29,14 @@ public class RegistrationController {
     model.addAttribute("userDTO", userDTO);
     model.addAttribute("confirm", "НА ВАШ ТЕЛЕФОН ОТПРАВЛЕН КОД ПРОВЕРКИ. ВВЕДИТЕ ЕГО В ПОЛЕ НИЖЕ");
     return "registration";
+  }
+
+  @ResponseBody
+  @PostMapping(path = "confirm", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public ApiResponse confirm(@RequestBody ConfirmPhoneDTO confirmPhoneDTO) {
+    return ApiResponse.builder()
+        .message(String.format("Телефон подтверждён %d", confirmPhoneDTO.getConfirmCode()))
+        .build();
   }
 
 }
