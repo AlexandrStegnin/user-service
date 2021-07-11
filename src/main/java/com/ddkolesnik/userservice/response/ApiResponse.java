7508902b31d4120e2188a128a5e32bcd4e3a1ab0
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-07-11 17:28:17 +0500

    Добавлен класс для отправки ответов с сервера

---
+++ src/main/java/com/ddkolesnik/userservice/response/ApiResponse.java
@@ -0,0 +1,22 @@
+package com.ddkolesnik.userservice.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * @author Aleksandr Stegnin on 11.07.2021
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ApiResponse {
+
+  String message;
+
+}
