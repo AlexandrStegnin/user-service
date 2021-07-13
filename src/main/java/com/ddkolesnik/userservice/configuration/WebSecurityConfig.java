1040cb499d023e1ec78c03baeb8070195d2d6053
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-07-13 11:04:38 +0500

    Добавлена форма входа для авторизации пользователя

--- src/main/java/com/ddkolesnik/userservice/configuration/WebSecurityConfig.java
+++ src/main/java/com/ddkolesnik/userservice/configuration/WebSecurityConfig.java
@@ -1,9 +1,12 @@
 package com.ddkolesnik.userservice.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author Aleksandr Stegnin on 08.07.2021
@@ -20,8 +23,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             "/", "/registration", "/confirm", "/success").permitAll()
         .anyRequest().authenticated()
         .and()
+        .formLogin()
+        .loginPage("/login")
+        .usernameParameter("username").passwordParameter("password")
+        .defaultSuccessUrl("/profile")
+        .permitAll()
+        .and()
         .logout()
         .permitAll();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 
 }
