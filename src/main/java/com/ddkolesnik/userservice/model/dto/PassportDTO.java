8883bad31d9e905223ee69a2132f4b69d0552f7d
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-07-13 23:00:44 +0500

    Добавлен паспорт

--- src/main/java/com/ddkolesnik/userservice/model/dto/PassportDTO.java
+++ src/main/java/com/ddkolesnik/userservice/model/dto/PassportDTO.java
@@ -8,24 +8,18 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 /**
- * @author Aleksandr Stegnin on 05.07.2021
+ * @author Aleksandr Stegnin on 13.07.2021
  */
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class UserDTO {
+public class PassportDTO {
 
-  Integer id;
-  String name;
-  String secondName;
-  String lastName;
-  String email;
-  String phone;
-  String confirmCode;
-  String inn;
-  String snils;
-  PassportDTO passport;
+  String serial;
+  String number;
+  String departmentCode;
+  String issuedBy;
 
 }
