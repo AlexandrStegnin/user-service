446cb4a1915ed6b2e9b1a86d4347e7a8665b68ac
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-12-01 23:12:53 +0500

    Тормозим отправку формы, чтобы сработало в Safari.

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -4,15 +4,17 @@ jQuery(document).ready(function ($) {
 
 function onSaveAdditionalFields() {
     let blocked = true
-    $('#details').on('submit', function (e) {
+    let details = $('#details')
+    details.on('submit', function (e) {
         if (noErrors()) {
             if (blocked) {
                 e.preventDefault()
                 showSuccessForm('Ваши данные успешно отправлены. Вам придет смс об аккредитации, после чего Вы сможете инвестировать.')
-                setTimeout(() => {
+                window.setTimeout(function () {
                     blocked = false
-                    $('#details').submit()
-                }, 2000)
+                    details.unbind('submit')
+                    $('#submit').click()
+                }, 1000)
             }
         } else {
             e.preventDefault()
