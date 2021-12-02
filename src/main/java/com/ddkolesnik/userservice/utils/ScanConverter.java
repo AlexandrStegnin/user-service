bb63600af6ad306ba18b6befc53ea6088621bc3e
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-12-02 13:05:13 +0500

    Добавлена зависимость + переделан конвертер, неправильно считал кол-во файлов

--- src/main/java/com/ddkolesnik/userservice/utils/ScanConverter.java
+++ src/main/java/com/ddkolesnik/userservice/utils/ScanConverter.java
@@ -4,13 +4,13 @@ import com.ddkolesnik.userservice.model.Scanned;
 import com.ddkolesnik.userservice.model.bitrix.file.FileData;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.util.Base64Utils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * @author Alexandr Stegnin
@@ -19,27 +19,30 @@ import java.util.Objects;
 @UtilityClass
 public class ScanConverter {
 
-  public boolean isScansAvailable(Scanned scanned) {
-    return Objects.nonNull(scanned)
-        && Objects.nonNull(scanned.getScans())
-        && (scanned.getScans().length > 0);
-  }
-
   public List<FileData> convertScans(Scanned dto) {
     MultipartFile[] files = dto.getScans();
     var fileDataList = new ArrayList<FileData>();
     for (MultipartFile file : files) {
       try {
         byte[] content = file.getBytes();
-        var imageString = Base64Utils.encodeToString(content);
-        var fileData = new FileData();
-        fileData.setFiles(new String[]{file.getOriginalFilename(), imageString});
-        fileDataList.add(fileData);
+        if (content.length > 0) {
+          var imageString = Base64Utils.encodeToString(content);
+          var fileData = new FileData();
+          fileData.setFiles(new String[]{file.getOriginalFilename(), imageString});
+          fileDataList.add(fileData);
+        }
       } catch (IOException e) {
         log.error("Произошла ошибка {}", e.getMessage());
       }
     }
     return fileDataList;
+  }
+
+  public boolean isScansAvailable(Scanned scanned) {
+    if (ObjectUtils.anyNull(scanned, scanned.getScans())) {
+      return false;
+    }
+    return !convertScans(scanned).isEmpty();
   }
 
 }
